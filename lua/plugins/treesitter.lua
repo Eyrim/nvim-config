9135edc26996d6b97618af8b58d7e0f19c8094f5
Eyrim <marfxedbot@gmail.com>
2024-02-20 00:27:58 +0000

    Move config into after/ add textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,21 +2,8 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        config = function()
-            -- If on windows, use clang
-            if vim.loop.os_uname().sysname == "Windows_NT" then
-                require("nvim-treesitter.install").compilers = { "clang" }
-            end
-
-            local config = require("nvim-treesitter.configs")
-            config.setup({
-                ensure_installed = {
-                    "markdown_inline" -- Markdown (experimental)
-                },
-                auto_install = true,
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
-        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects"
     },
 }
