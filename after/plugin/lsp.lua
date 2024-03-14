2608abe821232208a60d2bd55bad4009ceb98ab6
Eyrim <marfxedbot@gmail.com>
2024-03-14 10:42:44 +0000

    When auto setting up LSP server, auto add completion

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,31 +3,36 @@ local mason_lspconfig = require("mason-lspconfig")
 local nvim_lspconfig = require("lspconfig")
 
 -- See :h mason-lspconfig.setup_handlers()
-mason_lspconfig.setup_handlers {
-    -- The first entry (without a key) will be the default handler
-    -- and will be called for each installed server that doesn't have
-    -- a dedicated handler.
-    function(server_name) -- Default handler
-        require("lspconfig")[server_name].setup {}
-    end,
-    -- Next, you can provide a dedicated handler for specific servers.
-    -- For example, a handler override for the `rust_analyzer`:
-    --["rust_analyzer"] = function ()
-    --    require("rust-tools").setup {}
-    --end
-    ["lua_ls"] = function ()
-        nvim_lspconfig.lua_ls.setup {
-            settings = {
-                Lua = {
-                    diagnostics = {
-                        globals = { "vim" }
-                    }
-                }
-            }
-        }
-    end,
-}
+mason_lspconfig.setup_handlers({
+	-- The first entry (without a key) will be the default handler
+	-- and will be called for each installed server that doesn't have
+	-- a dedicated handler.
+	function(server_name) -- Default handler
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+		require("lspconfig")[server_name].setup({
+			capabilities = capabilities,
+		})
+	end,
+	-- Next, you can provide a dedicated handler for specific servers.
+	-- For example, a handler override for the `rust_analyzer`:
+	--["rust_analyzer"] = function ()
+	--    require("rust-tools").setup {}
+	--
+	["lua_ls"] = function()
+		nvim_lspconfig.lua_ls.setup({
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+				},
+			},
+		})
+	end,
+})
+
+vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
