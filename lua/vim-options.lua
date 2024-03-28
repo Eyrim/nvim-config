e9eed9d689e48d7667907bd88793bae59e2111a0
Eyrim <marfxedbot@gmail.com>
2024-03-28 10:13:52 +0000

    Set relative and absolute line numbers

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,5 +4,7 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
 
+vim.cmd("set rnu")
 vim.cmd("set number")
 vim.cmd("set signcolumn=yes")
+vim.cmd("set cursorline")
